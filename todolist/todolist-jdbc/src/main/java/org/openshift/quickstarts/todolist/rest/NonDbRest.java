9b50585a8749cbd82ab70bd87ff0de54aee8d287
robinjohnhopkins <robinjohnhopkins@hotmail.com>
2020-03-21 20:38:02 +0000

    rest env files

--- todolist/todolist-jdbc/src/main/java/org/openshift/quickstarts/todolist/rest/NonDbRest.java
+++ todolist/todolist-jdbc/src/main/java/org/openshift/quickstarts/todolist/rest/NonDbRest.java
@@ -2,13 +2,22 @@ package org.openshift.quickstarts.todolist.rest;
 
 
 
+import org.openshift.quickstarts.todolist.service.Find;
+
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Stream;
 
 @Path("rest")
@@ -37,6 +46,39 @@ public class NonDbRest {
                 map(String::valueOf).
                 limit(10);
         numberStream.forEach(array::add);
+        return array.build();
+    }
+
+    @Path("/env")
+    @GET
+    public JsonArray env() {
+        JsonArrayBuilder array = Json.createArrayBuilder();
+        Map<String, String> env = System.getenv();
+        // Java 8
+        env.forEach((k, v) -> array.add(k + ":" + v));
+        return array.build();
+    }
+
+    @Path("/files")
+    @GET
+    public JsonArray files(@QueryParam("dir") String dir,
+                           @QueryParam("pattern") String pattern) throws IOException {
+
+        JsonArrayBuilder array = Json.createArrayBuilder();
+        if (dir == null || dir.length() == 0){
+            dir = "/";
+        }
+        if (pattern == null || pattern.length() == 0){
+            pattern = "*";
+        }
+        List<String> listFileStrings = new ArrayList<>();
+        java.nio.file.Path startingDir = Paths.get(dir);
+        Find.Finder finder = new Find.Finder(pattern, listFileStrings);
+        Files.walkFileTree(startingDir, finder);
+        finder.done();
+
+        // Java 8
+        listFileStrings.forEach((k) -> array.add(k));
         return array.build();
     }
 
